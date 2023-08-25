bca86745508073072da7f5f4408ba248ece7cb9a
Eduarda Alves <65419192+EduardaAlv@users.noreply.github.com>
2023-08-25 14:36:47 -0300

    Identity

---
+++ Alura/popular/02_DML_DATA_identity.sql
@@ -0,0 +1,6 @@
+--Auto Incremento
+-- O Identity cria um auto incremento no id, o primeiro valor é de onde vai começar (1, -), o segundo é de quanto ele vai incrementar a cada dado inserido.
+CREATE TABLE TAB_IDENTITY (
+    ID INT IDENTITY(1,1) NOT NULL, 
+    DESCRITOR VARCHAR (20) NULL
+);
