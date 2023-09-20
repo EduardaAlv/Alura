8123799c49a12572d369d629330081bedb23fbd9
Eduarda Alves <65419192+EduardaAlv@users.noreply.github.com>
2023-09-20 15:31:03 -0300

    Estudos sobre o tsql

---
+++ Alura/trilha_sql/t-sql/functions_udf.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION CalculaDesconto (@CODIGO_PRODUTO VARCHAR(10), @DescontoPercentual INT)
+RETURNS FLOAT
+AS
+BEGIN
+   DECLARE @Desconto FLOAT
+   DECLARE @PrecoComDesconto FLOAT
+   DECLARE @Preco FLOAT
+   SELECT @Preco = [PREÇO DE LISTA] FROM [TABELA DE PRODUTOS] WHERE [CODIGO DO PRODUTO] = @CODIGO_PRODUTO
+   SET @Desconto = (@Preco * @DescontoPercentual) / 100
+   SET @PrecoComDesconto = @Preco - @Desconto
+   RETURN @PrecoComDesconto
+END
